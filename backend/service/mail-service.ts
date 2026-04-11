349666712a2a5d804d95084016ac28f6a2c318ac
Alex <sasha.nepron@gmail.com>
2026-04-11 19:36:48 +0300

    mail service fixes

--- backend/service/mail-service.ts
+++ backend/service/mail-service.ts
@@ -1,40 +1,33 @@
-import nodemailer, { type Transporter } from 'nodemailer';
+import { Resend } from 'resend';
 import ApiError from '../exceptions/api-errors.js';
 
 class MailService {
-    transporter: Transporter;
+    private resend: Resend;
+
     constructor() {
-        const host = process.env.SMTP_HOST;
-        const port = process.env.SMTP_PORT;
-        const user = process.env.SMTP_USER;
-        const pass = process.env.SMTP_PASS;
-        console.log(host, port, user, pass);
-        if (host && port && user && pass) {
-            this.transporter = nodemailer.createTransport({
-                host: host,
-                port: Number(port),
-                secure: Number(port) === 465,
-                auth: {
-                    user: user,
-                    pass: pass,
-                },
-            });
-        } else throw ApiError.BadRequest('Проблемы с SMTP конфигурацией');
+        if (!process.env.RESEND_API_KEY) {
+            throw ApiError.BadRequest('No RESEND_API_KEY');
+        }
+
+        this.resend = new Resend(process.env.RESEND_API_KEY);
     }
+
     async sendActivationMail(email: string, activationLink: string) {
-        const user = process.env.SMTP_USER;
         try {
-            await this.transporter.sendMail({
-                from: user,
+            await this.resend.emails.send({
+                from: 'onboarding@resend.dev',
                 to: email,
-                subject: 'Активация аккаунта на ' + process.env.API_URL,
-                text: '',
-                html: `<div>
-                <h1>Для активации перейдите по ссылке</h1>
-                <a href="${activationLink}">${activationLink}</a>
-            </div>`,
+                subject: 'Активация аккаунта',
+                html: `
+                    <div>
+                        <h1>Активация аккаунта</h1>
+                        <p>Перейдите по ссылке:</p>
+                        <a href="${activationLink}">${activationLink}</a>
+                    </div>
+                `,
             });
-        } catch {
+        } catch (e) {
+            console.log(e);
             throw ApiError.BadRequest('Ошибка отправки письма');
         }
     }
